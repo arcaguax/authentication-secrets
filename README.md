6afd21a6d858a4c1cdbff4d21b39c6f15799eb3e
Andrew <arcabezudo@dons.usfca.edu>
2022-03-31 20:56:16 -0700

    Level 3 upgrade

--- README.md
+++ README.md
@@ -2,8 +2,16 @@
 
 live deployment: 
 
-Practicing of Authenication & Security. Using database encryption, salting, hashing, Cookies,
-and Google OAuth 2.0
+Practicing of Authenication & Security. 
+
+Levels of Security: 
+
+Level 1: User names and passwords
+Level 2: Database Encryption and env files
+Level 3: Hashing Passwords
+Level 4: Salting and Hassing Passwords
+Level 5: Cookies and Sessions
+Level 6: OAuth 2.0
 
 ### Tech Stack: 
 MERN w/ mongoose.
--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const ejs = require("ejs");
 const app = express();
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
+
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
@@ -21,8 +22,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
 
 
 const User = new mongoose.model("User", userSchema);
@@ -42,7 +42,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
     const newUser = new User ({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err) {
         if (err) {
